8df0f85880e52634530506fede6e1a8b15d3fb60
Laband <lukeabandeen@gmail.com>
2024-03-08 11:57:55 -0500

    progress

---
+++ src/services/cats.js
@@ -0,0 +1,46 @@
+import api from "./apiConfig.js";
+
+export const getCats = async () => {
+    try {
+        const response = await api.get("/cats");
+        return response.data;
+    } catch (error) {
+        console.error("Error Getting all Cats: ", error);
+    }
+};
+
+export const getCat = async (id) => {
+    try {
+        const response = await api.get(`/cats/${id}`)
+        return response.data;
+    } catch (error) {
+        console.error("Error Getting Cat: ", error);
+    }
+};
+
+export const createCat = async (catData) => {
+    try {
+        const response = await api.post("/cats", catData);
+        return response.data;
+    } catch (error) {
+        console.error("Error Creating Cat: ", error);
+    }
+};
+
+export const editCat = async (id, catData) => {
+    try {
+        const response = await api.put(`/cats/${id}`, catData);
+        return response.data;
+    } catch (error) {
+        console.error("Error Updating Cat: ", error);
+    }
+};
+
+export const deleteCat = async (id) => {
+    try {
+        const response = await api.delete(`.cats.${id}`);
+        return response.data;
+    } catch (error) {
+        console.error("Error Deleting Cat: ", error);
+    }
+};
