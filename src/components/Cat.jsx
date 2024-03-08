74f9b2527ae8f139b8833585c54c3e6a868eeb22
Laband <lukeabandeen@gmail.com>
2024-03-08 15:38:46 -0500

    idk

--- src/components/Cat.jsx
+++ src/components/Cat.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 
 function Cat({cat}) {
+  console.log(cat)
   return (
     <div>
-      <Link to={`/cats/${cat.id}`}>
+      <Link to={`/cats/${cat._id}`}>
         <img className="cat-images" src={cat.image} alt={cat.name} />
       </Link>
     </div>
